c4ea3200733772e30b76939dc8a580fcd1198545
Bilal <40690623+Lasfe@users.noreply.github.com>
2019-11-06 14:42:25 +0200

    Create Hafta #8 Görevi 3.cpp

---
+++ Hafta #8 Görevi 3.cpp
@@ -0,0 +1,32 @@
+//Fonksiyon ile ax+b=0 denkleminin çözümü
+#include <iostream> 
+#include <cmath> 
+using namespace std; 
+
+double Kok(int,int);		//fonksiyon prototipi veya imzası (signature) 
+int main() 
+{ 
+  setlocale(LC_ALL,"Turkish"); 
+  int a,b; 
+
+  cout<<"Sırasıyla a ve b değerlerini giriniz : "<<endl; 
+  cin>>a>>b;
+  
+  while(a==0)
+  {
+  	cout<<"a değeri 0 olursa denklemi çözemem.Lüften a değerini tekrar gir.";
+  	cin>>a;
+  }
+	
+cout<<"Denklemi Kökü: "<<Kok(a,b)<<endl; 
+
+return 0; 
+} 
+
+//fonksiyon tanımı 
+
+
+double Kok(int a, int b) 
+{ 
+return -b/a; 
+}
