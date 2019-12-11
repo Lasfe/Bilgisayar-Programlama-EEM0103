983b9c9fe2d4be4bdf7b23ac4bba581f810623da
Bilal <40690623+Lasfe@users.noreply.github.com>
2019-12-11 17:47:27 +0300

    Create Hafta #12 Görevi 5.cpp

---
+++ Hafta #12 Görevi 5.cpp
@@ -0,0 +1,19 @@
+//Diziler (Arrays).
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+  	setlocale(LC_ALL,"Turkish");
+  	int n[10];
+    
+  for(int i=0;i<10;i++)
+	n[i]=50+rand()%6;
+	cout<<"Eleman"<<setw(13)<<"Değer"<<endl;
+	
+	for(int j=0;j<10;j++)
+	cout<<setw(6)<<j+1<<setw(13)<<n[j]<<endl;
+	
+return 0;
+}
