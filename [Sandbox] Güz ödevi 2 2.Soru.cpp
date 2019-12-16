50a5342f2997c73e7eef0143a900b845a2d61936
Bilal <40690623+Lasfe@users.noreply.github.com>
2019-12-16 09:28:46 +0300

    Create [Sandbox] Güz ödevi 2 2.Soru.cpp

---
+++ [Sandbox] Güz ödevi 2 2.Soru.cpp
@@ -0,0 +1,42 @@
+//Soru 2 Palindrom kelimeler.
+#include <iostream>
+using namespace std;
+
+int Palindrom(string, int, int);	//Bu kodda Fonksiyon yardımı ile palindrom bulacağız.
+
+int main()
+{
+	setlocale(LC_ALL,"Turkish");
+	int sonKarakter;
+	string kelime;
+	
+	cout << "Bir kelime girin: ";	//Kelimenin nokta ile bitmesine gerek yok.
+	cin >>kelime;
+	
+	sonKarakter = kelime.length();	//En önemli kısım.
+	Palindrom(kelime, 0, sonKarakter-1);	//Fonksiyonu çağırma.
+}
+
+int Palindrom(string kelime, int ilkKarakter, int sonKarakter)	//Fonksiyonumuz.
+{
+	if(kelime[ilkKarakter] == kelime[sonKarakter])	//Kontroller
+	{
+		if(sonKarakter > ilkKarakter)
+		{
+		Palindrom(kelime, ilkKarakter+1, sonKarakter-1);
+		}
+		else
+		{
+		cout << "'" << kelime << "' Bu kelimenin tersten okunuşu aynıdır!"; 
+		}
+	}
+	
+	else
+	{
+	cout << "'" << kelime << "' Bu kelimenin tersten okunuşu aynı değildir!";
+	}
+	
+	cout << "\n\n";
+	
+return main();
+}
