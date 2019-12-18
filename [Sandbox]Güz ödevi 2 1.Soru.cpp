87eab1a20b0ab76d2c7937acdaa6ed103476bf5a
Bilal <40690623+Lasfe@users.noreply.github.com>
2019-12-18 16:10:43 +0300

    Update [Sandbox]Güz ödevi 2 1.Soru.cpp

--- [Sandbox]Güz ödevi 2 1.Soru.cpp
+++ [Sandbox]Güz ödevi 2 1.Soru.cpp
@@ -45,3 +45,48 @@ int Buyukluk(int a)	//En büyük ve en küçük elemanın tesbiti.
 {
 
 }
+
+/*
+//Soru 2 Palindrom kelimeler.
+#include <iostream>
+using namespace std;
+
+int Palindrom(string, int, int);	//Bu kodda Fonksiyon yardımı ile palindrom bulacağız.
+
+int main()
+{
+	setlocale(LC_ALL,"Turkish");
+	int sonKarakter;
+	string kelime;
+	
+	cout << "Bir kelime girin: ";	//Kelimenin nokta ile bitmesine gerek yok.
+	cin >>kelime;
+	
+	sonKarakter = kelime.length();	//Kelimenin uzunluğu
+	Palindrom(kelime, 0, sonKarakter-1);
+}
+
+int Palindrom(string kelime, int ilkKarakter, int sonKarakter)	//Fonksiyonumuz.
+{
+	if(kelime[ilkKarakter] == kelime[sonKarakter])	//Kontroller
+	{
+		if(sonKarakter > ilkKarakter)
+		{
+		Palindrom(kelime, ilkKarakter+1, sonKarakter-1);
+		}
+		else
+		{
+		cout <<"Bu kelimenin tersten okunuşu aynıdır!"; 
+		}
+	}
+	
+	else
+	{
+	cout <<"Bu kelimenin tersten okunuşu aynı değildir!";
+	}
+	
+	cout << "\n\n";
+	
+return main();
+}
+*/
