1a7c481317fa24587793295b5a49cc59038c2a99
Bilal <40690623+Lasfe@users.noreply.github.com>
2019-12-18 16:24:09 +0300

    Create Hafta #13 Görevi 3.cpp

---
+++ Hafta #13 Görevi 3.cpp
@@ -0,0 +1,11 @@
+//Constant Variables(Sabit değişkenler).
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL,"Turkish");
+	const int X=7;	//İlk değer atanmış sabit değişken.
+	cout<<"X sabit değişkeninin değeri = "<<X<<endl;
+return 0;
+}
