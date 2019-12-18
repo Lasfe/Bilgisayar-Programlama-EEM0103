323cf6c1ca7e2abd38e92645ca3a19cc1981ef46
Bilal <40690623+Lasfe@users.noreply.github.com>
2019-12-18 16:26:22 +0300

    Create Hafta #13 Görevi 8[Fix Required].cpp

---
+++ Hafta #13 Görevi 8[Fix Required].cpp
@@ -0,0 +1,20 @@
+//0-20 arası çift sayılardan oluşan dizi.[Need Fix]
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL,"Turkish");
+	int n[20];
+  
+	for(int i=0;i<20;i++)
+	n[i]=;
+  
+	cout<<"Eleman"<<setw(13)<<"Değer"<<endl;
+	
+	for(int j=0;j<20;j++)
+	cout<<setw(6)<<j+1<<setw(13)<<n[j]<<endl;
+	
+return 0;
+}
