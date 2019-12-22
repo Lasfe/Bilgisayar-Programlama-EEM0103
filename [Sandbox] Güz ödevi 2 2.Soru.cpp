c73171286c5a6cddcac1ebfab0c325c7fba52bee
Bilal <40690623+Lasfe@users.noreply.github.com>
2019-12-22 16:39:59 +0300

    Update [Sandbox] Güz ödevi 2 2.Soru.cpp

--- [Sandbox] Güz ödevi 2 2.Soru.cpp
+++ [Sandbox] Güz ödevi 2 2.Soru.cpp
@@ -2,47 +2,55 @@
 //Tersten okununup okunmadığını kontrol et.
 //Sonucu ekrana yazdır.
 
+// 2.Soru
 #include <iostream>
+
 using namespace std;
 
-int Palindrom(string, int, int);	//Bu kodda Fonksiyon yardımı ile palindrom bulacağız.
+int Palindrom(string, int, int);
 
 int main()
 {
 	setlocale(LC_ALL,"Turkish");
 	
-	int sonKarakter;	//Son karakter için değişken.
-	string kelime;	//Kelimenin variablesi.
+	int sonKarakter;
+	string kelime;
 	
-	cout << "Bir kelime girin: ";	//Kelimenin nokta ile bitmesine gerek yok.
-	cin >>kelime;
+	cout<<"Kontrol etmek istediğiniz kelimeyi giriniz : ";
+	cin>>kelime;
 	
-	sonKarakter = kelime.length();	//Kelimenin uzunluğu
-	Palindrom(kelime, 0, sonKarakter-1);	//Fonksiyonu çağırma.
+	sonKarakter=kelime.length();
+	Palindrom(kelime, 0, sonKarakter-1);
 }
 
 
-int Palindrom(string kelime, int ilkKarakter, int sonKarakter)	//Fonksiyonumuz.
+int Palindrom(string kelime, int ilkKarakter, int sonKarakter)
 {
-	if(kelime[ilkKarakter] == kelime[sonKarakter])	//Kontroller
+	if(kelime[ilkKarakter] == kelime[sonKarakter])
 	{
-		if(sonKarakter > ilkKarakter)
+		if(sonKarakter>ilkKarakter)
 		{
-		Palindrom(kelime, ilkKarakter+1, sonKarakter-1);	//Kelimenin sağından ve solundan harf kontrolü.
+		Palindrom(kelime, ilkKarakter+1, sonKarakter-1);
 		}
 		
 		else
 		{
-		cout<<"Bu kelimenin tersten okunuşu aynıdır !";	//Doğru sonuç.
+		cout<<"Bu kelimenin tersten okunuşu aynıdır !";
 		}
 	}
 	
 	else
 	{
-	cout<<"Bu kelimenin tersten okunuşu aynı değildir !";	//Yanlış sonuç.
+	cout<<"Bu kelimenin tersten okunuşu aynı değildir !";
 	}
 	
 	cout << "\n\n";
 	
-return main();	//Sürekli tekrar etsin.
+return main();
 }
+
+// Önemli Not = Büyük ve Küçük Harf duyarlılığı yoktur !
+
+// Bilal BAĞCIOĞLU
+// 19332629062
+// Elektrik-Elektronik Mühendisliği
